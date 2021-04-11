a428a24e8b15d1c4e87bdd41dc696dbf0798184a
kevinfreire <kfreirea95@gmail.com>
2021-04-11 15:37:25 -0400

    Updates

---
+++ precache-manifest.077c164ea80a17c723c11073a59d5992.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a4233496dd16edc19d74d35615e06e",
+    "url": "/mind-over-matter/index.html"
+  },
+  {
+    "revision": "7dda259b0ad1fcfcc99c",
+    "url": "/mind-over-matter/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "584253c34b1e11324da1",
+    "url": "/mind-over-matter/static/js/2.f9155b10.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/mind-over-matter/static/js/2.f9155b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dda259b0ad1fcfcc99c",
+    "url": "/mind-over-matter/static/js/main.ef6e2c26.chunk.js"
+  },
+  {
+    "revision": "eeb3c6fd21663c422fc5",
+    "url": "/mind-over-matter/static/js/runtime-main.5693c926.js"
+  },
+  {
+    "revision": "d193634d0757f6b75b999da40da8a1cd",
+    "url": "/mind-over-matter/static/media/12-rules-of-life-cover.d193634d.jpg"
+  },
+  {
+    "revision": "6024687e47a57010054d59e61f33e26a",
+    "url": "/mind-over-matter/static/media/a-force-for-good-cover.6024687e.jpg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/mind-over-matter/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/mind-over-matter/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/mind-over-matter/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/mind-over-matter/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/mind-over-matter/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "3f118119ec5cebedef568dc2a6ba07da",
+    "url": "/mind-over-matter/static/media/bg5.3f118119.jpg"
+  },
+  {
+    "revision": "8858c53ac4e40b3795c19e3c12a4a132",
+    "url": "/mind-over-matter/static/media/bg6.8858c53a.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/mind-over-matter/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "e7d5d83d45af182feaa67142b930c171",
+    "url": "/mind-over-matter/static/media/brain-maker-cover.e7d5d83d.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/mind-over-matter/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/mind-over-matter/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/mind-over-matter/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/mind-over-matter/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "a58958a586d89c117e2663787e37e1f3",
+    "url": "/mind-over-matter/static/media/kevin.a58958a5.JPG"
+  },
+  {
+    "revision": "065a6b55bbb1ec5eddc92e49ae002547",
+    "url": "/mind-over-matter/static/media/kevinBack.065a6b55.JPG"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/mind-over-matter/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "8699d9750f1a38f3170821dd8fcab45c",
+    "url": "/mind-over-matter/static/media/lion-cover.8699d975.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/mind-over-matter/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "fcfb58992c7b0525bc58bd1c9a911717",
+    "url": "/mind-over-matter/static/media/miko.fcfb5899.jpg"
+  },
+  {
+    "revision": "e9dec081ce413c4ebe412c3f5a028765",
+    "url": "/mind-over-matter/static/media/obiwan.e9dec081.JPG"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/mind-over-matter/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/mind-over-matter/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/mind-over-matter/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/mind-over-matter/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/mind-over-matter/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/mind-over-matter/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/mind-over-matter/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/mind-over-matter/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/mind-over-matter/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "6d370eb39b1a40f05c57deeae0389f90",
+    "url": "/mind-over-matter/static/media/the-grain-brain-cover.6d370eb3.jpg"
+  },
+  {
+    "revision": "89eeb3cccddd6e9172138845bb983172",
+    "url": "/mind-over-matter/static/media/the-happiness-advantage-cover.89eeb3cc.jpg"
+  },
+  {
+    "revision": "f64f361c5142de5d2fc25b62d5d217fd",
+    "url": "/mind-over-matter/static/media/the-power-of-habit-cover.f64f361c.png"
+  },
+  {
+    "revision": "2609778e20a280cff53cf11a2e66946a",
+    "url": "/mind-over-matter/static/media/total-recall-cover.2609778e.jpg"
+  },
+  {
+    "revision": "da2b0e80fbb785c12caebfdd6e9fa94f",
+    "url": "/mind-over-matter/static/media/unbroken-cover.da2b0e80.jpg"
+  }
+]);
